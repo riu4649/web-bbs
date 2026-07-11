0ecafa92c597908389aef339116bde89749b9e8d
riu <rai10riu@gmail.com>
2026-07-11 14:22:09 +0900

    appdate

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 'use strict';
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
